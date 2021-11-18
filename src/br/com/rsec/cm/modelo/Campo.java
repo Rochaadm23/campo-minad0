0ce24db017195aa461b864dbd4ce1c4c1bfc1c02
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2021-11-18 01:57:08 -0300

    Foi feita a inserção dos atributos da classe Campo, e criado uma lista de vizinhos.

--- src/br/com/rsec/cm/modelo/Campo.java
+++ src/br/com/rsec/cm/modelo/Campo.java
@@ -1,4 +1,20 @@
 package br.com.rsec.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
+    private final int coluna;
+    private final int linha;
+
+    private boolean aberto = false;
+    private boolean minado = false;
+    private boolean marcado = false;
+
+    private List<Campo> vizinhos = new ArrayList<>();
+
+    public Campo(int coluna, int linha) {
+        this.coluna = coluna;
+        this.linha = linha;
+    }
 }
